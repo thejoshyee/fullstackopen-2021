8a33104fe17ba34880256ce8d60638ff1390a31e
Josh Yee <joshyee@gmail.com>
2022-02-23 23:22:25 -0800

    finished courseinfo

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,10 +1,42 @@
 import React from 'react'
 
-export default function Course(props) {
-    console.log(props)
+const Content = ({ exercises }) => {
+    return (
+        exercises.map(part => {
+            return <li key={part.id}>{part.name} {part.exercises}</li>
+        })
+    )
+}
+
+const Total = ({ exercises }) => {
+
+    //take exercises object and return new object with just exercises 
+    const totalExercises = exercises.reduce((total, number) => {
+        return { "exercises": total.exercises + number.exercises }
+    }).exercises
+    
+    return (
+        <p>
+            <b>Total Number of Exercises: {totalExercises}</b>
+        </p>
+    )
+}
+
+const Course = ({ course }) => { 
+    return (
+        <>
+            <h1>{course.name}</h1>
+            <Content exercises={course.parts} />
+            <Total exercises={course.parts}/>
+        </>
+    )
+}
+
+
+export default function Courses({ courses }) {
   return (
     <div>
-        {props.course.name}
+        {courses.map(course => <Course key={course.id} course={course} /> )}
     </div>
   )
 }
