047e8780e29aa82583a7220897395a7dfc3851c4
Josh Yee <joshyee@gmail.com>
2022-02-27 01:02:36 -0800

    added put method to json db

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -34,11 +34,31 @@ const App = (props) => {
         const noteObject = {
             content: newNote,
             date: new Date().toISOString(),
-            important: Math.random() < 0.5,
-            id: notes.length + 1
+            important: Math.random() < 0.5
         }
-        setNotes(notes.concat(noteObject))
-        setNewNote("")
+        // setNotes(notes.concat(noteObject))
+        // setNewNote("")
+        axios
+          .post('http://localhost:3001/notes', noteObject)
+          .then(response => {
+            setNotes(notes.concat(response.data))
+            setNewNote('')
+          })
+  }
+
+
+  const toggleImportanceOf = (id) => {
+    //url of the note of the id from user clicked
+    const url = `http://localhost:3001/notes/${id}`
+    // find the note that use clicked id with the id of the note in the notes array
+    const note = notes.find(n => n.id === id)
+    //new note object with prev and changed property
+    const changedNote = { ...note, important: !note.important}
+
+    axios.put(url, changedNote).then(response => {
+      console.log(response.data)
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
   }
 
   
@@ -53,7 +73,12 @@ const App = (props) => {
       
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+            
+            />
         )}
       </ul>
         <form onSubmit={addNote}>
