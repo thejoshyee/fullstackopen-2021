719a53cb07963b20c7a3c34c7ca9f0fc6916fd10
Josh Yee <joshyee@gmail.com>
2022-03-27 16:59:50 -0700

    tests

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,66 @@ const dummy = blogs => {
     return 1
 }
 
+const totalLikes = blogs => {
+    if (blogs.length === 0) {
+        return 0
+    }
+    if (blogs.length === 1) {
+        return blogs[0].likes
+    }
+    const reduced = blogs.reduce((acc, item) => {
+        return { likes: acc.likes + item.likes }
+    })
+    return reduced.likes
+}
+
+const favoriteBlog = blogs => {
+    const bestBlog = blogs.reduce((allBlogs, currentBlog) => 
+        allBlogs.likes > currentBlog.likes 
+            ? allBlogs 
+            : currentBlog
+    )
+    return bestBlog
+}
+
+const mostBlogs = blogs => {
+    const authors = {}
+    for (let entry of blogs) {
+        if (authors[entry.author]) {
+            authors[entry.author] += 1
+        } else {
+            authors[entry.author] = 1
+        }
+    }
+
+    const mostBlogsAuthor = Object.keys(authors).reduce((acc, current) => authors[acc] > authors[current] ? acc : current)
+    return {
+        author: mostBlogsAuthor,
+        entries: authors[mostBlogsAuthor]
+    }
+
+}
+
+const mostLikes = blogs => {
+    const authors = {}
+    for (let entry of blogs) {
+        if (authors[entry.author]) {
+            authors[entry.author] += entry.likes
+        } else {
+            authors[entry.author] = entry.likes
+        }
+    }
+    const mostLikedauthor = Object.keys(authors).reduce((acc, current) => authors[acc] > authors[current] ? acc : current)
+    return {
+        author: mostLikedauthor,
+        likes: authors[mostLikedauthor]
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
