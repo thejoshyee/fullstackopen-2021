57a2fc0a74ea239c425bdcf2efa55fa886ce3a57
Josh Yee <joshyee@gmail.com>
2022-04-15 10:55:59 -0700

    added token auth login

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -6,6 +6,8 @@ const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -14,6 +16,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
 
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,6 +1,8 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -16,12 +18,25 @@ blogsRouter.delete('/:id', async (request, response) => {
     response.status(204).end()
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
-    const user = await User.findById(body.userId)
-
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    
     if(!body.title || !body.url) return response.status(400).json({ error: 'title or url is missing' })
 
     const blog = new Blog({
