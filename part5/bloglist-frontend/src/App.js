a0b2fe4ac092be7eabd362090868012ba67f4395
Josh Yee <joshyee@gmail.com>
2022-04-18 23:38:52 -0700

    added add new blogs function

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -5,16 +5,31 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setURL] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      //set token
+      blogService.setToken(user.token)
+    }
+  },[])
 
   const loginForm = () => (
     <form onSubmit={handleLogin}>
@@ -40,6 +55,15 @@ const App = () => {
     </form>      
   )
 
+  const logout = () => (
+    <button type="submit" onClick={logoutUser}>logout</button>
+  )
+
+  const logoutUser = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
 
   const handleLogin = async e => {
     e.preventDefault()
@@ -48,6 +72,12 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -58,18 +88,78 @@ const App = () => {
         }, 5000)
     }
   }
-console.log(user)
+
+  const handleMessage = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage('')
+    }, 5000)
+  }
+
+  
+  const blogForm = () => (
+
+    <form onSubmit={handleSave}>
+      
+      <p>title: <input
+        value={title}
+        onChange={({ target }) => setTitle(target.value)}
+      />
+      </p>
+
+      <p>author: <input
+        value={author}
+        onChange={({ target }) => setAuthor(target.value)}
+      />
+      </p>
+
+      <p>url: <input
+        value={url}
+        onChange={({ target }) => setURL(target.value)}
+      />
+      </p>
+
+      <button type="submit">Save</button>
+    </form>  
+  )
+
+  const handleSave = async (event) => {
+    event.preventDefault()
+    createBlog({ title: title, author: author, url: url })
+  }
+
+  const createBlog = async (entry) => {
+    try {
+      const newBlog = await blogService.create(entry)
+      setBlogs([...blogs, newBlog])
+      handleMessage(newBlog.title + 'has been created')
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+
   return (
     <div>
       <h2>Dope Blogs</h2>
-      
+      <h2>{errorMessage}</h2>
       {user === null ?
       loginForm() :
       <div>
-        <p>{user.name} logged-in</p>
-        {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+        <p>{user.name} logged-in {logout()}</p>
+
+        <div>
+          {blogForm()}
+        </div>
+
+        <div>
+          
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />)
+          }
+          
+        </div>
+
       </div>
     }
 
