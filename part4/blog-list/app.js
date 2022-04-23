e5341f15189773130d366de936a6510166147b88
Josh Yee <joshyee@gmail.com>
2022-04-23 16:30:52 -0700

    added cypress testing

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -24,6 +24,11 @@ app.use(middleware.userExtractor)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
