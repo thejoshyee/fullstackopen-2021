ab761b535425b581768494187f315fd5be0a006e
Josh Yee <joshyee@gmail.com>
2022-05-13 18:53:05 -0700

    update

--- postgres-test/index.js
+++ postgres-test/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
+
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
@@ -11,27 +12,71 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
         }
       },
     })
+
+
+    class Note extends Model {}
+        Note.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        important: {
+            type: DataTypes.BOOLEAN
+        },
+        date: {
+            type: DataTypes.DATE
+        }
+        }, {
+        sequelize,
+        underscored: true,
+        timestamps: false,
+        modelName: 'note'
+    })
     
 
-    // const main = async () => {
-    //     try {
-    //       await sequelize.authenticate()
-    //       const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
-    //       console.log(notes)
-    //       sequelize.close()
-    //     } catch (error) {
-    //       console.error('Unable to connect to the database:', error)
-    //     }
-    //   }
-      
-
-    //   main()
+    Note.sync()
 
     app.get('/api/notes', async (req, res) => {
-        const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+        const notes = await Note.findAll()
         res.json(notes)
       })
+      
+      app.use(express.json())
+      
+      app.post('/api/notes', async (req, res) => {
+        try {
+          const note = await Note.create(req.body)
+          return res.json(note)
+        } catch (error) {
+          return res.status(400).json({ error })
+        }
+      })
+      
+      app.get('/api/notes/:id', async (req, res) => {
+        const note = await Note.findByPk(req.params.id)
+        if (note) {
+            console.log(note.toJSON())
+          res.json(note)
+        } else {
+          res.status(404).end()
+        }
+      })
+
+      app.get('/', async (req, res) => {
+        const notes = await Note.findAll()
+      
+        console.log(JSON.stringify(notes, null, 2))
+      
+        res.json(notes)
+      })
+
       const PORT = process.env.PORT || 3001
       app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`)
       })
+
